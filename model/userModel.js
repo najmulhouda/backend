41fa8fcac441f5ff24d7bea86f3e0207137ce09d
najmulhouda <najmulhouda6@gmail.com>
2024-03-04 09:44:16 +0600

    backend fileg

---
+++ model/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    fname:{
+        type: String,
+        required: true
+    },
+    lname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    position:{
+        type: String,
+        required: true
+    },
+    number:{
+        type: Number,
+        required: true
+    },
+    status:{
+        type: String,
+        block: false
+    }
+
+})
+
+
+export default mongoose.model("User", userSchema);
